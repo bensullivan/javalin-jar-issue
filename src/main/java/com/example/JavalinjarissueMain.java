b09af014f164ae95a3e01b724b59a52c42348be4
Ben Sullivan <office@bensullivan.net>
2024-11-11 17:24:56 +1000

    Format code

--- src/main/java/com/example/JavalinjarissueMain.java
+++ src/main/java/com/example/JavalinjarissueMain.java
@@ -6,11 +6,12 @@ import io.javalin.http.staticfiles.Location;
 public class JavalinjarissueMain {
 
     public static void main(String[] args) {
-        Javalin.create(config -> config.staticFiles.add(staticFiles -> {
-                    staticFiles.hostedPath = "/";
-                    staticFiles.directory = "/web";
-                    staticFiles.location = Location.CLASSPATH;
-                }))
+        Javalin.create(config ->
+                        config.staticFiles.add(staticFiles -> {
+                            staticFiles.hostedPath = "/";
+                            staticFiles.directory = "/web";
+                            staticFiles.location = Location.CLASSPATH;
+                        }))
                 .get("/hello", ctx -> ctx.result("Hello World"))
                 .start(7070);
     }
